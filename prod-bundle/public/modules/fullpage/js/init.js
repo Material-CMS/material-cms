de6488dff9c71882947a5c19369a02af5e100e79
Felix <f.langenberg@thurm.de>
2018-05-28 14:11:12 +0200

    Added minified css production bundle

---
+++ prod-bundle/public/modules/fullpage/js/init.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  // A more complex initialization with all options set could look like this
+  // further information on:
+  // https://github.com/alvarotrigo/fullPage.js/.
+
+	//if you see this is broswer dev tools its defenitly working!
+	$('#fullpage').fullpage({
+		//Navigation
+		menu: '.nav-activator',
+		lockAnchors: false,
+		anchors:['page01', 'page02', 'page03', 'page04'],
+		navigation: false,
+		navigationPosition: 'right',
+		navigationTooltips: ['slide01', 'slide02'],
+		showActiveTooltip: false,
+		slidesNavigation: false,
+		slidesNavPosition: 'bottom',
+
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 600,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: false,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: true,
+		verticalCentered: true,
+		sectionsColor : ['#383e59', '#38c0d3', '#0d0d0d', '#8c0d3b'],
+		paddingTop: '3em',
+		paddingBottom: '10px',
+		fixedElements: '#header, .footer',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.slide',
+
+		lazyLoading: true,
+
+		//events
+		onLeave: function(index, nextIndex, direction){},
+		afterLoad: function(anchorLink, index){},
+		afterRender: function(){},
+		afterResize: function(){},
+		afterResponsive: function(isResponsive){},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+
+	});
+
+});
---
+++ prod-bundle/public/modules/my-apostrophe-assets/js/site.js
@@ -0,0 +1,11 @@
+$(function() {
+  // Start with your project-level client-side javascript here.
+  // JQuery and lodash (_) are both included with Apostrophe, so no need to
+  // worry about including them on your own.
+
+  $(function(){
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+  });
+
+});
