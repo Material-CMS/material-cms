6fe1a11f91c16beea2c0889eb3c00b78ddc30fb7
felix <felix.lberg@gmail.com>
2018-06-02 18:50:21 +0200

    Removed minifi of assets becuase of developing

--- prod-bundle/public/modules/materialize-events-widgets/js/init.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  // A more complex initialization with all options set could look like this
-  // further information on:
-  // https://github.com/alvarotrigo/fullPage.js/.
-	$('.collapsible').collapsible();
-});
