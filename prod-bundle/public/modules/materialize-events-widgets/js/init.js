b8456aeb2e4e59bf01a16846c4c491479aedf06d
felix <felix.lberg@gmail.com>
2018-06-02 18:53:51 +0200

    And minifying again...

---
+++ prod-bundle/public/modules/materialize-events-widgets/js/init.js
@@ -0,0 +1,6 @@
+$(function() {
+  // A more complex initialization with all options set could look like this
+  // further information on:
+  // https://github.com/alvarotrigo/fullPage.js/.
+	$('.collapsible').collapsible();
+});
