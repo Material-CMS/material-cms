eb598336272a1499a2e254c7c0a930c73de17e41
felix <felix.lberg@gmail.com>
2018-06-03 00:15:38 +0200

    fixed bugs and vulnerabilities

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ var apos = require('apostrophe')({
     'apostrophe-site-map': {
       excludeTypes: []
     },
+    'apostrophe-optimizer': {
+      stats: false,
+      debug: false
+    },
     //Templates
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
   }
