2bc2d7d649bfb242e59bed3d05cb35799a0b5102
felix <felix.lberg@gmail.com>
2018-06-09 16:04:03 +0200

    added core-js as dependencies to fix bug with 1.x.x and apostrophe-favicons

--- app.js
+++ app.js
@@ -31,6 +31,22 @@ var apos = require('apostrophe')({
       stats: false,
       debug: false
     },
+    'apostrophe-favicons': {},
+    'apostrophe-favicons-global': {
+      destinationDir: '/fav/',
+      // Defaults to `/favicons/`. This is an uploadfs path, it will become /uploads/favicons/ on a server
+
+      tempDir: 'temp',
+      // Defaults to `tempAposFavicons`. Directory where files are temporarily written before being passed to uploadfs. This is your root project directory. Omit leading slash
+
+      faviconConfig : {
+        icons: {
+          windows: false
+        }
+      }
+      // Configuration for favicon module, see options here https://github.com/evilebottnawi/favicons#usage
+      // **NOTE** The `path` option is automatically figured out by the module, no need to set it.
+    },
     //Templates
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
   }
