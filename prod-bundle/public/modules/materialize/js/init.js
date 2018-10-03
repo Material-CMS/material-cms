94279ec0827168c96996db1269b1e9d413d69f68
Felix <felix.lberg@gmail.com>
2018-10-04 01:19:20 +0200

    small fixes and performance

--- prod-bundle/public/modules/materialize/js/init.js
+++ prod-bundle/public/modules/materialize/js/init.js
@@ -1,7 +1,21 @@
 //Materialize Components Jquery initializations
 $(function(){
-  $('.sidenav').sidenav();
-  $('.collapsible').collapsible();
+  // Collasible init and Options
+  var elem = document.querySelector('.collapsible');
+  var instance = new M.Collapsible(elem, {
+    // accordion:	true
+  });
+  // Sidenav init and Options
+  var elem = document.querySelector('.sidenav');
+  var instance = new M.Sidenav(elem, {
+   draggable: true,
+   preventScrolling: true
+  });
+  // Modal init and Options
+  var elem = document.querySelector('.modal');
+  var instance = new M.Modal(elem, {
+   preventScrolling: true
+  });
+  // Tabs
   $('.tabs').tabs();
-  $('.modal').modal();
 });
