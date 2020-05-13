203574208270f266c4c64cc7d3adb483430415f9
harald <p4y4s0@protonmail.com>
2020-05-13 17:22:21 +0200

    small code formatting

--- local.example.js
+++ local.example.js
@@ -5,10 +5,6 @@
 module.exports = {
   baseUrl: 'http://localhost:3000',
   modules: {
-    'apostrophe-assets': {
-      // Set to true for full CSS and JS minify, on staging and production servers
-      // minify: true
-    },
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
