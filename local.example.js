110b05a8cbf962309b356db7e365d6fe83e54e67
harald <p4y4s0@protonmail.com>
2020-05-16 03:26:40 +0200

    small cleaning

--- local.example.js
+++ local.example.js
@@ -5,6 +5,27 @@
 module.exports = {
   baseUrl: 'http://localhost:3000',
   modules: {
+    // Apostrophe has full-text search capabilities built in
+    // https://docs.apostrophecms.org/core-concepts/apostrophe-search/search.html
+    'apostrophe-search': {
+      types: [
+        'home'
+      ]
+    },
+    // Add parked page for search fuctions. Parked pages
+    // configures certain pages to be automatically created
+    // and refreshed whenever the site starts up.
+    'apostrophe-pages': {
+      park: [
+        {
+          title: 'Search',
+          slug: '/search',
+          type: 'apostrophe-search',
+          label: 'Search',
+          published: true
+        }
+      ]
+    },
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
@@ -12,6 +33,10 @@ module.exports = {
       // There is legacy support for host, port, name, user and password options,
       // but this is not necessary. They can all go in the uri option like this:
       // mongodb://user:password@host:port/dbname
+    },
+    // Change default Port
+    'apostrophe-express': {
+      port: 3001
     },
     // Minify Html
     'apostrophe-templates': {
