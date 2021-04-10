2cc49d40c54b9bf37bac0ae5b666d7a05f413b47
Felix <p4y4s0@protonmail.com>
2021-04-10 14:28:55 +0200

    moved apostrophe-express secret to local.js

--- local.example.js
+++ local.example.js
@@ -5,50 +5,13 @@
 module.exports = {
   baseUrl: 'http://localhost:3000',
   modules: {
-    // Apostrophe has full-text search capabilities built in
-    // https://docs.apostrophecms.org/core-concepts/apostrophe-search/search.html
-    'apostrophe-search': {
-      types: [
-        'home'
-      ]
-    },
-    // Add parked page for search fuctions. Parked pages
-    // configures certain pages to be automatically created
-    // and refreshed whenever the site starts up.
-    'apostrophe-pages': {
-      park: [
-        {
-          title: 'Search',
-          slug: '/search',
-          type: 'apostrophe-search',
-          label: 'Search',
-          published: true
-        }
-      ]
-    },
-    // If these are your db settings then you don't need to be explicit. If not
-    // you can uncomment this and get more specific.
-    'apostrophe-db': {
-      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
-      // There is legacy support for host, port, name, user and password options,
-      // but this is not necessary. They can all go in the uri option like this:
-      // mongodb://user:password@host:port/dbname
-    },
-    // Change default Port
+    // Set a Secre for your Session
+    // https://docs.apostrophecms.org/howtos/storing-sessions-in-redis.html#what-about-caches
     'apostrophe-express': {
-      port: 3001
-    },
-    // Minify Html
-    'apostrophe-templates': {
-      minify: (process.env.ENV === 'prod') // minify: true
-    },
-    // Minify Assets
-    'apostrophe-assets': {
-      minify: (process.env.ENV === 'prod') // minify: true
-    },
-    // DB performance test tool
-    'apostrophe-profiler': {
-    },
+      session: {
+        secret: 'put-your-secret-here'
+      }
+    }
     // Email Settings for nodemailer
     'apostrophe-email': {
       nodemailer: {
@@ -60,6 +23,50 @@ module.exports = {
             pass: 'SMTP_PW' // or use: process.env.SMTP_PW
         }
       }
+    }
+    // Apostrophe has full-text search capabilities built set searched pages here
+    // https://docs.apostrophecms.org/core-concepts/apostrophe-search/search.html
+    'apostrophe-search': {
+      // types: [
+      //   'home'
+      // ]
+    },
+    // Add parked page for search fuctions. Parked pages
+    // configures certain pages to be automatically created
+    // and refreshed whenever the site starts up.
+    'apostrophe-pages': {
+      // park: [
+      //   {
+      //     title: 'Search',
+      //     slug: '/search',
+      //     type: 'apostrophe-search',
+      //     label: 'Search',
+      //     published: true
+      //   }
+      // ]
+    },
+    // If these are your db settings then you don't need to be explicit. If not
+    // you can uncomment this and get more specific.
+    'apostrophe-db': {
+      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
+      // There is legacy support for host, port, name, user and password options,
+      // but this is not necessary. They can all go in the uri option like this:
+      // mongodb://user:password@host:port/dbname
+    },
+    // Change default Port
+    'apostrophe-express': {
+      // port: 3001
+    },
+    // Minify Html
+    'apostrophe-templates': {
+      minify: (process.env.ENV === 'prod') // minify: true
+    },
+    // Minify Assets
+    'apostrophe-assets': {
+      minify: (process.env.ENV === 'prod') // minify: true
+    },
+    // DB performance test tool
+    'apostrophe-profiler': {
     }
   }
 };
