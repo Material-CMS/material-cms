aada53e50609fd9075ec4d9a33191e93737003c8
kafkiano <34920220+kafkiano@users.noreply.github.com>
2026-01-14 22:21:51 +0100

    small refactoring

--- deploy/dev.config.js
+++ deploy/dev.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps: [{
     script: 'app.js',
     output: 'data/temp/dev.log',
-    error: 'data/temp/dev.log',
-    time: true
+    error: 'data/temp/dev.log'
   }]
 };
--- deploy/prod.config.js
+++ deploy/prod.config.js
@@ -3,7 +3,6 @@ module.exports = {
     script: 'app.js',
     output: 'data/temp/prod.log',
     error: 'data/temp/prod.log',
-    time: true,
     env: {
       NODE_ENV: 'prod'
     }
